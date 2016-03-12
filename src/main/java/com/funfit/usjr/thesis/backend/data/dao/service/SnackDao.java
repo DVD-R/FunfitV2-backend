9dbf9f92f578edfe5ebd6863a0c175fafb629b64
andalesv <victorandalezz@gmail.com>
2016-03-12 11:46:09 +0800

    additional table for meals

---
+++ src/main/java/com/funfit/usjr/thesis/backend/data/dao/service/SnackDao.java
@@ -0,0 +1,8 @@
+package com.funfit.usjr.thesis.backend.data.dao.service;
+
+import com.funfit.usjr.thesis.backend.data.dao.GenericDao;
+import com.funfit.usjr.thesis.backend.models.Snack;
+
+public interface SnackDao extends GenericDao<Snack>{
+
+}
